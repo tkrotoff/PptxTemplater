4ddeccfda38447fad35bb2acac988d184df4a189
Tanguy Krotoff <tkrotoff@gmail.com>
2012-09-05 16:58:33 +0200

    Get all the text in all slides See http://msdn.microsoft.com/en-us/library/office/gg278331

---
+++ PptxTemplating.Tests/PptxTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PptxTemplating.Tests
+{
+    [TestClass]
+    public class PptxTest
+    {
+        // See How to: Get All the Text in All Slides in a Presentation http://msdn.microsoft.com/en-us/library/office/gg278331
+        [TestMethod]
+        public void TestGetAllTextInAllSlides()
+        {
+            string file = "../../files/test1.pptx";
+            int numberOfSlides = Pptx.CountSlides(file);
+            System.Console.WriteLine("Number of slides = {0}", numberOfSlides);
+            string slideText;
+            for (int i = 0; i < numberOfSlides; i++)
+            {
+                Pptx.GetSlideIdAndText(out slideText, file, i);
+                System.Console.WriteLine("Slide #{0} contains: {1}", i + 1, slideText);
+            }
+        }
+    }
+}
--- PptxTemplating/Pptx.cs
+++ PptxTemplating/Pptx.cs
@@ -2,10 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Presentation;
+using A = DocumentFormat.OpenXml.Drawing;
 
 namespace PptxTemplating
 {
     public class Pptx
     {
+        // See How to: Get All the Text in All Slides in a Presentation http://msdn.microsoft.com/en-us/library/office/gg278331
+        public static int CountSlides(string presentationFile)
+        {
+            // Open the presentation as read-only.
+            using (PresentationDocument presentationDocument = PresentationDocument.Open(presentationFile, false))
+            {
+                // Pass the presentation to the next CountSlides method
+                // and return the slide count.
+                return CountSlides(presentationDocument);
+            }
+        }
+
+        // Count the slides in the presentation.
+        // See How to: Get All the Text in All Slides in a Presentation http://msdn.microsoft.com/en-us/library/office/gg278331
+        public static int CountSlides(PresentationDocument presentationDocument)
+        {
+            // Check for a null document object.
+            if (presentationDocument == null)
+            {
+                throw new ArgumentNullException("presentationDocument");
+            }
+
+            int slidesCount = 0;
+
+            // Get the presentation part of document.
+            PresentationPart presentationPart = presentationDocument.PresentationPart;
+            // Get the slide count from the SlideParts.
+            if (presentationPart != null)
+            {
+                slidesCount = presentationPart.SlideParts.Count();
+            }
+            // Return the slide count to the previous method.
+            return slidesCount;
+        }
+
+        // See How to: Get All the Text in All Slides in a Presentation http://msdn.microsoft.com/en-us/library/office/gg278331
+        public static void GetSlideIdAndText(out string sldText, string docName, int index)
+        {
+            using (PresentationDocument ppt = PresentationDocument.Open(docName, false))
+            {
+                // Get the relationship ID of the first slide.
+                PresentationPart part = ppt.PresentationPart;
+                OpenXmlElementList slideIds = part.Presentation.SlideIdList.ChildElements;
+
+                string relId = (slideIds[index] as SlideId).RelationshipId;
+
+                // Get the slide part from the relationship ID.
+                SlidePart slide = (SlidePart)part.GetPartById(relId);
+
+                // Build a StringBuilder object.
+                StringBuilder paragraphText = new StringBuilder();
+
+                // Get the inner text of the slide:
+                IEnumerable<A.Text> texts = slide.Slide.Descendants<A.Text>();
+                foreach (A.Text text in texts)
+                {
+                    paragraphText.Append(text.Text);
+                }
+                sldText = paragraphText.ToString();
+            }
+        }
     }
 }
