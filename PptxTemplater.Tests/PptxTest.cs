b4c8ba75601f5feb1c0b3deb9596692e29132af8
Tanguy Krotoff <tkrotoff@gmail.com>
2012-10-30 19:26:26 +0100

    Improve ReplaceTablesInAllSlides

--- PptxTemplater.Tests/PptxTest.cs
+++ PptxTemplater.Tests/PptxTest.cs
@@ -418,8 +418,31 @@
         [TestMethod]
         public void ReplaceTablesInAllSlides()
         {
-            const string srcFileName = "../../files/ReplaceTablesInAllSlides.pptx";
             const string dstFileName = "../../files/ReplaceTablesInAllSlides_output.pptx";
+
+            this.ReplaceTablesInAllSlides(dstFileName, 0, 0, 0);
+            this.AssertPptxEquals(dstFileName, 2, "HELLO!  ");
+
+            this.ReplaceTablesInAllSlides(dstFileName, 10, 0, 0);
+            this.AssertPptxEquals(dstFileName, 6, "HELLO! Table1 Col2 Col3 Col4 Col5 Col6 1.0.1 1.0.2 1.0.3 1.0.4 1.0.5 1.0.6 1.1.1 1.1.2 1.1.3 1.1.4 1.1.5 1.1.6 1.2.1 1.2.2 1.2.3 1.2.4 1.2.5 1.2.6 Table2 Col2 Col3 Col4 Col5 Col6 Table3 Col2 Col3 Col4 Col5 Col6 Table1 Col2 Col3 Col4 Col5 Col6 1.3.1 1.3.2 1.3.3 1.3.4 1.3.5 1.3.6 1.4.1 1.4.2 1.4.3 1.4.4 1.4.5 1.4.6 1.5.1 1.5.2 1.5.3 1.5.4 1.5.5 1.5.6 Table2 Col2 Col3 Col4 Col5 Col6 Table3 Col2 Col3 Col4 Col5 Col6 Table1 Col2 Col3 Col4 Col5 Col6 1.6.1 1.6.2 1.6.3 1.6.4 1.6.5 1.6.6 1.7.1 1.7.2 1.7.3 1.7.4 1.7.5 1.7.6 1.8.1 1.8.2 1.8.3 1.8.4 1.8.5 1.8.6 Table2 Col2 Col3 Col4 Col5 Col6 Table3 Col2 Col3 Col4 Col5 Col6 Table1 Col2 Col3 Col4 Col5 Col6 1.9.1 1.9.2 1.9.3 1.9.4 1.9.5 1.9.6 Table2 Col2 Col3 Col4 Col5 Col6 Table3 Col2 Col3 Col4 Col5 Col6  ");
+
+            this.ReplaceTablesInAllSlides(dstFileName, 0, 10, 0);
+            this.AssertPptxEquals(dstFileName, 5, "HELLO! Table1 Col2 Col3 Col4 Col5 Col6 {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} Table2 Col2 Col3 Col4 Col5 Col6 2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.1.1 2.1.2 2.1.3 2.1.4 2.1.5 2.1.6 2.2.1 2.2.2 2.2.3 2.2.4 2.2.5 2.2.6 2.3.1 2.3.2 2.3.3 2.3.4 2.3.5 2.3.6 Table3 Col2 Col3 Col4 Col5 Col6 Table1 Col2 Col3 Col4 Col5 Col6 {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} Table2 Col2 Col3 Col4 Col5 Col6 2.4.1 2.4.2 2.4.3 2.4.4 2.4.5 2.4.6 2.5.1 2.5.2 2.5.3 2.5.4 2.5.5 2.5.6 2.6.1 2.6.2 2.6.3 2.6.4 2.6.5 2.6.6 2.7.1 2.7.2 2.7.3 2.7.4 2.7.5 2.7.6 Table3 Col2 Col3 Col4 Col5 Col6 Table1 Col2 Col3 Col4 Col5 Col6 {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} Table2 Col2 Col3 Col4 Col5 Col6 2.8.1 2.8.2 2.8.3 2.8.4 2.8.5 2.8.6 2.9.1 2.9.2 2.9.3 2.9.4 2.9.5 2.9.6 Table3 Col2 Col3 Col4 Col5 Col6  ");
+
+            this.ReplaceTablesInAllSlides(dstFileName, 0, 0, 10);
+            this.AssertPptxEquals(dstFileName, 5, "HELLO! Table1 Col2 Col3 Col4 Col5 Col6 {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} Table2 Col2 Col3 Col4 Col5 Col6 {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} Table3 Col2 Col3 Col4 Col5 Col6 3.0.1 3.0.2 3.0.3 3.0.4 3.0.5 3.0.6 3.1.1 3.1.2 3.1.3 3.1.4 3.1.5 3.1.6 3.2.1 3.2.2 3.2.3 3.2.4 3.2.5 3.2.6 3.3.1 3.3.2 3.3.3 3.3.4 3.3.5 3.3.6 Table1 Col2 Col3 Col4 Col5 Col6 {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} Table2 Col2 Col3 Col4 Col5 Col6 {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} Table3 Col2 Col3 Col4 Col5 Col6 3.4.1 3.4.2 3.4.3 3.4.4 3.4.5 3.4.6 3.5.1 3.5.2 3.5.3 3.5.4 3.5.5 3.5.6 3.6.1 3.6.2 3.6.3 3.6.4 3.6.5 3.6.6 3.7.1 3.7.2 3.7.3 3.7.4 3.7.5 3.7.6 Table1 Col2 Col3 Col4 Col5 Col6 {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} Table2 Col2 Col3 Col4 Col5 Col6 {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} {{cell1}} {{cell2}} {{cell3}} {{cell4}} {{cell5}} {{cell6}} Table3 Col2 Col3 Col4 Col5 Col6 3.8.1 3.8.2 3.8.3 3.8.4 3.8.5 3.8.6 3.9.1 3.9.2 3.9.3 3.9.4 3.9.5 3.9.6  ");
+
+            this.ReplaceTablesInAllSlides(dstFileName, 10, 10, 10);
+            this.AssertPptxEquals(dstFileName, 6, "HELLO! Table1 Col2 Col3 Col4 Col5 Col6 1.0.1 1.0.2 1.0.3 1.0.4 1.0.5 1.0.6 1.1.1 1.1.2 1.1.3 1.1.4 1.1.5 1.1.6 1.2.1 1.2.2 1.2.3 1.2.4 1.2.5 1.2.6 Table2 Col2 Col3 Col4 Col5 Col6 2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.1.1 2.1.2 2.1.3 2.1.4 2.1.5 2.1.6 2.2.1 2.2.2 2.2.3 2.2.4 2.2.5 2.2.6 2.3.1 2.3.2 2.3.3 2.3.4 2.3.5 2.3.6 Table3 Col2 Col3 Col4 Col5 Col6 3.0.1 3.0.2 3.0.3 3.0.4 3.0.5 3.0.6 3.1.1 3.1.2 3.1.3 3.1.4 3.1.5 3.1.6 3.2.1 3.2.2 3.2.3 3.2.4 3.2.5 3.2.6 3.3.1 3.3.2 3.3.3 3.3.4 3.3.5 3.3.6 Table1 Col2 Col3 Col4 Col5 Col6 1.3.1 1.3.2 1.3.3 1.3.4 1.3.5 1.3.6 1.4.1 1.4.2 1.4.3 1.4.4 1.4.5 1.4.6 1.5.1 1.5.2 1.5.3 1.5.4 1.5.5 1.5.6 Table2 Col2 Col3 Col4 Col5 Col6 2.4.1 2.4.2 2.4.3 2.4.4 2.4.5 2.4.6 2.5.1 2.5.2 2.5.3 2.5.4 2.5.5 2.5.6 2.6.1 2.6.2 2.6.3 2.6.4 2.6.5 2.6.6 2.7.1 2.7.2 2.7.3 2.7.4 2.7.5 2.7.6 Table3 Col2 Col3 Col4 Col5 Col6 3.4.1 3.4.2 3.4.3 3.4.4 3.4.5 3.4.6 3.5.1 3.5.2 3.5.3 3.5.4 3.5.5 3.5.6 3.6.1 3.6.2 3.6.3 3.6.4 3.6.5 3.6.6 3.7.1 3.7.2 3.7.3 3.7.4 3.7.5 3.7.6 Table1 Col2 Col3 Col4 Col5 Col6 1.6.1 1.6.2 1.6.3 1.6.4 1.6.5 1.6.6 1.7.1 1.7.2 1.7.3 1.7.4 1.7.5 1.7.6 1.8.1 1.8.2 1.8.3 1.8.4 1.8.5 1.8.6 Table2 Col2 Col3 Col4 Col5 Col6 2.8.1 2.8.2 2.8.3 2.8.4 2.8.5 2.8.6 2.9.1 2.9.2 2.9.3 2.9.4 2.9.5 2.9.6 Table3 Col2 Col3 Col4 Col5 Col6 3.8.1 3.8.2 3.8.3 3.8.4 3.8.5 3.8.6 3.9.1 3.9.2 3.9.3 3.9.4 3.9.5 3.9.6 Table1 Col2 Col3 Col4 Col5 Col6 1.9.1 1.9.2 1.9.3 1.9.4 1.9.5 1.9.6 Table2 Col2 Col3 Col4 Col5 Col6 Table3 Col2 Col3 Col4 Col5 Col6  ");
+
+            this.ReplaceTablesInAllSlides(dstFileName, 11, 22, 33);
+            this.AssertPptxEquals(dstFileName, 11, "HELLO! Table1 Col2 Col3 Col4 Col5 Col6 1.0.1 1.0.2 1.0.3 1.0.4 1.0.5 1.0.6 1.1.1 1.1.2 1.1.3 1.1.4 1.1.5 1.1.6 1.2.1 1.2.2 1.2.3 1.2.4 1.2.5 1.2.6 Table2 Col2 Col3 Col4 Col5 Col6 2.0.1 2.0.2 2.0.3 2.0.4 2.0.5 2.0.6 2.1.1 2.1.2 2.1.3 2.1.4 2.1.5 2.1.6 2.2.1 2.2.2 2.2.3 2.2.4 2.2.5 2.2.6 2.3.1 2.3.2 2.3.3 2.3.4 2.3.5 2.3.6 Table3 Col2 Col3 Col4 Col5 Col6 3.0.1 3.0.2 3.0.3 3.0.4 3.0.5 3.0.6 3.1.1 3.1.2 3.1.3 3.1.4 3.1.5 3.1.6 3.2.1 3.2.2 3.2.3 3.2.4 3.2.5 3.2.6 3.3.1 3.3.2 3.3.3 3.3.4 3.3.5 3.3.6 Table1 Col2 Col3 Col4 Col5 Col6 1.3.1 1.3.2 1.3.3 1.3.4 1.3.5 1.3.6 1.4.1 1.4.2 1.4.3 1.4.4 1.4.5 1.4.6 1.5.1 1.5.2 1.5.3 1.5.4 1.5.5 1.5.6 Table2 Col2 Col3 Col4 Col5 Col6 2.4.1 2.4.2 2.4.3 2.4.4 2.4.5 2.4.6 2.5.1 2.5.2 2.5.3 2.5.4 2.5.5 2.5.6 2.6.1 2.6.2 2.6.3 2.6.4 2.6.5 2.6.6 2.7.1 2.7.2 2.7.3 2.7.4 2.7.5 2.7.6 Table3 Col2 Col3 Col4 Col5 Col6 3.4.1 3.4.2 3.4.3 3.4.4 3.4.5 3.4.6 3.5.1 3.5.2 3.5.3 3.5.4 3.5.5 3.5.6 3.6.1 3.6.2 3.6.3 3.6.4 3.6.5 3.6.6 3.7.1 3.7.2 3.7.3 3.7.4 3.7.5 3.7.6 Table1 Col2 Col3 Col4 Col5 Col6 1.6.1 1.6.2 1.6.3 1.6.4 1.6.5 1.6.6 1.7.1 1.7.2 1.7.3 1.7.4 1.7.5 1.7.6 1.8.1 1.8.2 1.8.3 1.8.4 1.8.5 1.8.6 Table2 Col2 Col3 Col4 Col5 Col6 2.8.1 2.8.2 2.8.3 2.8.4 2.8.5 2.8.6 2.9.1 2.9.2 2.9.3 2.9.4 2.9.5 2.9.6 2.10.1 2.10.2 2.10.3 2.10.4 2.10.5 2.10.6 2.11.1 2.11.2 2.11.3 2.11.4 2.11.5 2.11.6 Table3 Col2 Col3 Col4 Col5 Col6 3.8.1 3.8.2 3.8.3 3.8.4 3.8.5 3.8.6 3.9.1 3.9.2 3.9.3 3.9.4 3.9.5 3.9.6 3.10.1 3.10.2 3.10.3 3.10.4 3.10.5 3.10.6 3.11.1 3.11.2 3.11.3 3.11.4 3.11.5 3.11.6 Table1 Col2 Col3 Col4 Col5 Col6 1.9.1 1.9.2 1.9.3 1.9.4 1.9.5 1.9.6 1.10.1 1.10.2 1.10.3 1.10.4 1.10.5 1.10.6 Table2 Col2 Col3 Col4 Col5 Col6 2.12.1 2.12.2 2.12.3 2.12.4 2.12.5 2.12.6 2.13.1 2.13.2 2.13.3 2.13.4 2.13.5 2.13.6 2.14.1 2.14.2 2.14.3 2.14.4 2.14.5 2.14.6 2.15.1 2.15.2 2.15.3 2.15.4 2.15.5 2.15.6 Table3 Col2 Col3 Col4 Col5 Col6 3.12.1 3.12.2 3.12.3 3.12.4 3.12.5 3.12.6 3.13.1 3.13.2 3.13.3 3.13.4 3.13.5 3.13.6 3.14.1 3.14.2 3.14.3 3.14.4 3.14.5 3.14.6 3.15.1 3.15.2 3.15.3 3.15.4 3.15.5 3.15.6 Table1 Col2 Col3 Col4 Col5 Col6 1.9.1 1.9.2 1.9.3 1.9.4 1.9.5 1.9.6 1.10.1 1.10.2 1.10.3 1.10.4 1.10.5 1.10.6 Table2 Col2 Col3 Col4 Col5 Col6 2.16.1 2.16.2 2.16.3 2.16.4 2.16.5 2.16.6 2.17.1 2.17.2 2.17.3 2.17.4 2.17.5 2.17.6 2.18.1 2.18.2 2.18.3 2.18.4 2.18.5 2.18.6 2.19.1 2.19.2 2.19.3 2.19.4 2.19.5 2.19.6 Table3 Col2 Col3 Col4 Col5 Col6 3.16.1 3.16.2 3.16.3 3.16.4 3.16.5 3.16.6 3.17.1 3.17.2 3.17.3 3.17.4 3.17.5 3.17.6 3.18.1 3.18.2 3.18.3 3.18.4 3.18.5 3.18.6 3.19.1 3.19.2 3.19.3 3.19.4 3.19.5 3.19.6 Table1 Col2 Col3 Col4 Col5 Col6 1.9.1 1.9.2 1.9.3 1.9.4 1.9.5 1.9.6 1.10.1 1.10.2 1.10.3 1.10.4 1.10.5 1.10.6 Table2 Col2 Col3 Col4 Col5 Col6 2.20.1 2.20.2 2.20.3 2.20.4 2.20.5 2.20.6 2.21.1 2.21.2 2.21.3 2.21.4 2.21.5 2.21.6 Table3 Col2 Col3 Col4 Col5 Col6 3.20.1 3.20.2 3.20.3 3.20.4 3.20.5 3.20.6 3.21.1 3.21.2 3.21.3 3.21.4 3.21.5 3.21.6 3.22.1 3.22.2 3.22.3 3.22.4 3.22.5 3.22.6 3.23.1 3.23.2 3.23.3 3.23.4 3.23.5 3.23.6 Table1 Col2 Col3 Col4 Col5 Col6 1.9.1 1.9.2 1.9.3 1.9.4 1.9.5 1.9.6 1.10.1 1.10.2 1.10.3 1.10.4 1.10.5 1.10.6 Table2 Col2 Col3 Col4 Col5 Col6 2.20.1 2.20.2 2.20.3 2.20.4 2.20.5 2.20.6 2.21.1 2.21.2 2.21.3 2.21.4 2.21.5 2.21.6 Table3 Col2 Col3 Col4 Col5 Col6 3.24.1 3.24.2 3.24.3 3.24.4 3.24.5 3.24.6 3.25.1 3.25.2 3.25.3 3.25.4 3.25.5 3.25.6 3.26.1 3.26.2 3.26.3 3.26.4 3.26.5 3.26.6 3.27.1 3.27.2 3.27.3 3.27.4 3.27.5 3.27.6 Table1 Col2 Col3 Col4 Col5 Col6 1.9.1 1.9.2 1.9.3 1.9.4 1.9.5 1.9.6 1.10.1 1.10.2 1.10.3 1.10.4 1.10.5 1.10.6 Table2 Col2 Col3 Col4 Col5 Col6 2.20.1 2.20.2 2.20.3 2.20.4 2.20.5 2.20.6 2.21.1 2.21.2 2.21.3 2.21.4 2.21.5 2.21.6 Table3 Col2 Col3 Col4 Col5 Col6 3.28.1 3.28.2 3.28.3 3.28.4 3.28.5 3.28.6 3.29.1 3.29.2 3.29.3 3.29.4 3.29.5 3.29.6 3.30.1 3.30.2 3.30.3 3.30.4 3.30.5 3.30.6 3.31.1 3.31.2 3.31.3 3.31.4 3.31.5 3.31.6 Table1 Col2 Col3 Col4 Col5 Col6 1.9.1 1.9.2 1.9.3 1.9.4 1.9.5 1.9.6 1.10.1 1.10.2 1.10.3 1.10.4 1.10.5 1.10.6 Table2 Col2 Col3 Col4 Col5 Col6 2.20.1 2.20.2 2.20.3 2.20.4 2.20.5 2.20.6 2.21.1 2.21.2 2.21.3 2.21.4 2.21.5 2.21.6 Table3 Col2 Col3 Col4 Col5 Col6 3.32.1 3.32.2 3.32.3 3.32.4 3.32.5 3.32.6  ");
+        }
+
+        private void ReplaceTablesInAllSlides(string dstFileName, int table1NbRows, int table2NbRows, int table3NbRows)
+        {
+            const string srcFileName = "../../files/ReplaceTablesInAllSlides.pptx";
+
             File.Delete(dstFileName);
             File.Copy(srcFileName, dstFileName);
 
@@ -442,10 +465,8 @@
             List<PptxSlide> existingSlides = new List<PptxSlide>();
 
             {
-                // 10 lines
-                int nbLines = 10;
                 List<PptxTable.Cell[]> rows = new List<PptxTable.Cell[]>();
-                for (int i = 0; i < nbLines; i++)
+                for (int i = 0; i < table1NbRows; i++)
                 {
                     PptxTable.Cell[] row = new[]
                             {
@@ -460,7 +481,20 @@
                 }
 
                 PptxSlide lastSlide = pptx.GetSlide(1);
-                PptxSlide slideTemplate = lastSlide;
+                if (existingSlides.Count > 0)
+                {
+                    lastSlide = existingSlides.Last();
+                }
+                PptxSlide slideTemplate = lastSlide.Clone();
+                foreach (PptxSlide slide in existingSlides)
+                {
+                    PptxTable table = slide.FindTables("{{table1}}").FirstOrDefault();
+                    if (table != null)
+                    {
+                        List<PptxTable.Cell[]> remainingRows = table.SetRowsNoInsert(rows);
+                        rows = remainingRows;
+                    }
+                }
                 while (rows.Count > 0)
                 {
                     PptxSlide newSlide = slideTemplate.Clone();
@@ -475,15 +509,12 @@
                     lastSlide = newSlide;
                     existingSlides.Add(newSlide);
                 }
-
                 slideTemplate.Remove();
             }
 
             {
-                // 27 lines
-                const int nbLines = 27;
                 List<PptxTable.Cell[]> rows = new List<PptxTable.Cell[]>();
-                for (int i = 0; i < nbLines; i++)
+                for (int i = 0; i < table2NbRows; i++)
                 {
                     PptxTable.Cell[] row = new[]
                         {
@@ -497,7 +528,11 @@
                     rows.Add(row);
                 }
 
-                PptxSlide lastSlide = existingSlides.Last();
+                PptxSlide lastSlide = pptx.GetSlide(1);
+                if (existingSlides.Count > 0)
+                {
+                    lastSlide = existingSlides.Last();
+                }
                 PptxSlide slideTemplate = lastSlide.Clone();
                 foreach (PptxSlide slide in existingSlides)
                 {
@@ -526,10 +561,8 @@
             }
 
             {
-                // 50 lines
-                int nbLines = 50;
                 List<PptxTable.Cell[]> rows = new List<PptxTable.Cell[]>();
-                for (int i = 0; i < nbLines; i++)
+                for (int i = 0; i < table3NbRows; i++)
                 {
                     PptxTable.Cell[] row = new[]
                         {
@@ -543,7 +576,11 @@
                     rows.Add(row);
                 }
 
-                PptxSlide lastSlide = existingSlides.Last();
+                PptxSlide lastSlide = pptx.GetSlide(1);
+                if (existingSlides.Count > 0)
+                {
+                    lastSlide = existingSlides.Last();
+                }
                 PptxSlide slideTemplate = lastSlide.Clone();
                 foreach (PptxSlide slide in existingSlides)
                 {
@@ -571,9 +608,9 @@
                 slideTemplate.Remove();
             }
 
-            pptx.Close();
+            pptx.GetSlide(1).Remove();
 
-            //this.AssertPptxEquals(dstFileName, 7, "Table1 Col2 Col3 Col4 Col5 Col6 HELLO Hello, world! 1  Hello, world! 2 Hello, world! 3 Hello, world! 4 Hello, world! 5 Hello, world! 6 Hello, world! 1 Hello, world! 2 Hello, world! 3 Hello, world! 4 Hello, world! 5 Hello, world! 6 Hello, world! 1 Hello, world! 2 Hello, world! 3 Hello, world! 4 Hello, world! 5 Hello, world! 6 HELLO Hello, world! 1 Hello, world! 2 Hello, world! 3 Hello, world! 4 Hello, world! 5 Hello, world! 6 HELLO! Table1 Col2 Col3 Col4 Col5 Col6 HELLO Hello, world! 1  Hello, world! 2 Hello, world! 3 Hello, world! 4 Hello, world! 5 Hello, world! 6 Hello, world! 1 Hello, world! 2 Hello, world! 3 Hello, world! 4 Hello, world! 5 Hello, world! 6 Hello, world! 1 Hello, world! 2 Hello, world! 3 Hello, world! 4 Hello, world! 5 Hello, world! 6 HELLO Hello, world! 1 Hello, world! 2 Hello, world! 3 Hello, world! 4 Hello, world! 5 Hello, world! 6 HELLO! Table1 Col2 Col3 Col4 Col5 Col6 HELLO Hello, world! 1  Hello, world! 2 Hello, world! 3 Hello, world! 4 Hello, world! 5 Hello, world! 6 Hello, world! 1 Hello, world! 2 Hello, world! 3 Hello, world! 4 Hello, world! 5 Hello, world! 6 HELLO! Table2 Col2 Col3 Col4 Col5 Col6 Bonjour 1 Bonjour 2 Bonjour 3 Bonjour 4 Bonjour 5 Bonjour 6 Bonjour 1 Bonjour 2 Bonjour 3 Bonjour 4 Bonjour 5 Bonjour 6 Table3 Col2 Col3 Col4 Col5 Col6 Hola! 1 Hola! 2 Hola! 3 Hola! 4 Hola! 5 Hola! 6 Hola! 1 Hola! 2 Hola! 3 Hola! 4 Hola! 5 Hola! 6 Hola! 1 Hola! 2 Hola! 3 Hola! 4 Hola! 5 Hola! 6 Hola! 1 Hola! 2 Hola! 3 Hola! 4 Hola! 5 Hola! 6 Table2 Col2 Col3 Col4 Col5 Col6 Bonjour 1 Bonjour 2 Bonjour 3 Bonjour 4 Bonjour 5 Bonjour 6 Bonjour 1 Bonjour 2 Bonjour 3 Bonjour 4 Bonjour 5 Bonjour 6 Table3 Col2 Col3 Col4 Col5 Col6 Hola! 1 Hola! 2 Hola! 3 Hola! 4 Hola! 5 Hola! 6 Hola! 1 Hola! 2 Hola! 3 Hola! 4 Hola! 5 Hola! 6 Hola! 1 Hola! 2 Hola! 3 Hola! 4 Hola! 5 Hola! 6 Hola! 1 Hola! 2 Hola! 3 Hola! 4 Hola! 5 Hola! 6 Table2 Col2 Col3 Col4 Col5 Col6 Bonjour 1 Bonjour 2 Bonjour 3 Bonjour 4 Bonjour 5 Bonjour 6 Bonjour 1 Bonjour 2 Bonjour 3 Bonjour 4 Bonjour 5 Bonjour 6 Table3 Col2 Col3 Col4 Col5 Col6 Hola! 1 Hola! 2 Hola! 3 Hola! 4 Hola! 5 Hola! 6 Hola! 1 Hola! 2 Hola! 3 Hola! 4 Hola! 5 Hola! 6  ");
+            pptx.Close();
         }
 
         [TestMethod]
